784b76b45edbc8f865eea9d8e24ba9f822589e6f
Jonas Hermsmeier <jhermsmeier@googlemail.com>
2012-05-13 02:32:35 +0200

    Updated coding style.

--- api/2.0.js
+++ api/2.0.js
@@ -1,46 +1,44 @@
 
-'use strict';
-
-var url    = require( 'url' );
-var query  = require( 'querystring' );
-var https  = require( 'https' );
-var crypto = require( 'crypto' );
+var url    = require( 'url' )
+var query  = require( 'querystring' )
+var https  = require( 'https' )
+var crypto = require( 'crypto' )
 
 var OAuth = {
-  util: require( '../../util' )
+  util: require( './util' )
 };
 
-var Client = function( options ) {
+module.exports = function( options ) {
   
   // Lifetimes
-  var ACCESS_TOKEN_LIFETIME  = 3600;
-  var AUTH_CODE_LIFETIME     = 30;
-  var REFRESH_TOKEN_LIFETIME = 1209600;
-  
+  var ACCESS_TOKEN_LIFETIME  = 3600
+  var AUTH_CODE_LIFETIME     = 30
+  var REFRESH_TOKEN_LIFETIME = 1209600
+
   // Response types (Obtaining End-User Authorization)
-  [
+  ;[
     'token',
     'code',
     'code-and-token'
-  ];
-  
+  ]
+
   // Response types (Obtaining an Access Token)
-  [
+  ;[
     'authorization_code',
     'password',
     'assertion',
     'refresh_token',
     'none'
-  ];
-  
+  ]
+
   /*
    * Default options
    */
   var defaults = {
     base: '',
     token_name: 'oauth_token'
-  };
-  
+  }
+
   /*
    * Constructs a new Client object from
    * given credentials.
@@ -52,24 +50,27 @@ var Client = function( options ) {
    */
   function Client( consumer ) {
     
-    if( !(this instanceof Client) )
-      return new Client( consumer );
+    if( !(this instanceof Client) ) {
+      return new Client( consumer )
+    }
     
-    if( !consumer.id || !consumer.secret )
-      throw new Error( 'Consumer credentials required.' );
+    if( !consumer.id || !consumer.secret ) {
+      throw new Error( 'Consumer credentials required.' )
+    }
     
-    if( options != null )
-      delete options.version;
+    if( options != null ) {
+      delete options.version
+    }
     
-    this.consumer = consumer;
-    this.options = OAuth.util.extend(
+    this.consumer = consumer
+    this.options  = OAuth.util.extend(
       true,
       defaults,
       options
-    );
+    )
     
   }
-  
+
   Client.prototype = {
     
     /**
@@ -80,10 +81,6 @@ var Client = function( options ) {
     
     
     
-  };
+  }
   
-  return Client;
-  
-};
-
-module.exports = Client;
+}
--- api/util.js
+++ api/util.js
@@ -1,8 +1,6 @@
 
-'use strict';
-
-var __slice = Array.prototype.slice;
-var __toString = Object.prototype.toString;
+var __slice    = Array.prototype.slice
+var __toString = Object.prototype.toString
 
 module.exports = {
   
@@ -14,13 +12,14 @@ module.exports = {
    */
   getNonce: function( length ) {
     
-    var chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789',
-        nonce = '';
+    var chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+    var nonce = ''
     
-    for( var i = 0; i < length; i++ )
-      nonce += chars[ ( Math.random() * length ) | 0 ];
+    for( var i = 0; i < length; i++ ) {
+      nonce += chars[ ( Math.random() * length ) | 0 ]
+    }
     
-    return nonce;
+    return nonce
     
   },
   
@@ -30,7 +29,7 @@ module.exports = {
    * @return {Number} timestamp
    */
   getTime: function() {
-    return ( new Date().getTime() / 1000 ) | 0;
+    return ( new Date().getTime() / 1000 ) | 0
   },
     
   /*
@@ -41,15 +40,16 @@ module.exports = {
    */
   encodeComponent: function( input ) {
     
-    if( input == null || input == '' )
-      return '';
+    if( input == null || input == '' ) {
+      return ''
+    }
     
-    input = encodeURIComponent( input );
+    input = encodeURIComponent( input )
     input = input.replace( /[!'()*]/g, function( char ) {
-      return '%' + char.charCodeAt(0).toString(16);
+      return '%' + char.charCodeAt(0).toString(16)
     })
     
-    return input;
+    return input
     
   },
   
@@ -61,10 +61,11 @@ module.exports = {
    */
   decodeComponent: function( input ) {
     
-    if( input != null )
-      input = input.replace( /\+/g, ' ' );
+    if( input != null ) {
+      input = input.replace( /\+/g, ' ' )
+    }
     
-    return decodeURIComponent( input );
+    return decodeURIComponent( input )
     
   },
   
@@ -75,8 +76,8 @@ module.exports = {
    * @return {String} type
    */
   type: function( value ) {
-    value = __toString.call( value );
-    return value.substring( 8, value.length - 1 );
+    value = __toString.call( value )
+    return value.substring( 8, value.length - 1 )
   },
   
   /*
@@ -87,24 +88,24 @@ module.exports = {
    */
   extend: function() {
     
-    var options, name, src, copy, clone, isObject, isArray,
-        target = arguments[0] || {},
-        i = 0,
-        length = arguments.length,
-        deep = false;
+    var options, name, src, copy, clone, isObject, isArray
+    var target = arguments[0] || {}
+    var i      = 0
+    var length = arguments.length
+    var deep   = false
     
     // Handle a deep copy
     if( typeof target === 'boolean' ) {
-      deep = target;
-      target = arguments[1] || {};
+      deep   = target
+      target = arguments[1] || {}
       // skip deep and target args
-      i = 2;
+      i = 2
     }
     
     // Handle case when target is a string
     // or soemthing (possible in deep copy)
     if( typeof target !== 'object' && this.type( target ) !== 'Function' ) {
-      target = {};
+      target = {}
     }
     
     for( ; i < length; i++ ) {
@@ -113,34 +114,33 @@ module.exports = {
         // Extend the base object
         for( name in options ) {
           
-          src = target[ name ];
-          copy = options[ name ];
+          src  = target[ name ]
+          copy = options[ name ]
           
           // Prevent never-ending loop
-          if( target === copy )
-            continue;
+          if( target === copy ) { continue }
           
-          isObject = this.type( copy ) === 'Object';
-          isArray  = this.type( copy ) === 'Array';
+          isObject = this.type( copy ) === 'Object'
+          isArray  = this.type( copy ) === 'Array'
           
           // Recurse if we're merging plain objects or arrays
           if( deep && copy && isObject || isArray ) {
             
             if( isArray ) {
-              isArray = false;
-              clone = src && this.type( src ) === 'Array' ? src : [];
+              isArray = false
+              clone = src && this.type( src ) === 'Array' ? src : []
             }
             else {
-              clone = src && this.type( src ) === 'Object' ? src : {};
+              clone = src && this.type( src ) === 'Object' ? src : {}
             }
             
             // Never move original object, clone them
-            target[ name ] = this.extend( deep, clone, copy );
+            target[ name ] = this.extend( deep, clone, copy )
             
           }
           // Don't bring in undefined values
           else if( copy !== undefined ) {
-            target[ name ] = copy;
+            target[ name ] = copy
           }
           
         }
@@ -148,8 +148,8 @@ module.exports = {
     }
     
     // Return the modified object
-    return target;
+    return target
     
   }
   
-};
+}
