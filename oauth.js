1587a98f7dbc7b017311789db8916c84c197d12b
Jonas Hermsmeier <jhermsmeier@googlemail.com>
2012-06-17 22:56:19 +0200

    Simplified oauth.js

--- oauth.js
+++ oauth.js
@@ -1,17 +1,5 @@
 
-var OAuth = {
+module.exports = {
   1.0: require( './api/1.0' ),
   // 2.0: require( './api/2.0' ),
-}
-
-module.exports = function( version ) {
-  
-  version = parseFloat( version )
-  
-  if( !OAuth.hasOwnProperty( version ) ) {
-    throw new Error( 'OAuth version not implemented.' )
-  }
-  
-  return OAuth[ version ]
-  
 }
